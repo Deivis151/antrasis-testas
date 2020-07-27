2743502c510fbcb828dfa7c6e0826b3965169fc9
Deivis151 <jurkusdeividas64@gmail.com>
2020-07-27 08:57:48 +0300

    data and main js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+import data from './data.js';
+
+const months = [
+    'Sausis',
+    'Vasaris',
+    'Kovas',
+    'Balandis',
+    'Gegužė',
+    'Birželis',
+    'Liepa',
+    'Rugpjūtis',
+    'Rugsėjis',
+    'Spalis',
+    'Lapkritis',
+    'Gruodis'
+];
